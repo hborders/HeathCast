940f3cb3567950c897740e588aa6ecd187111b88
Heath Borders <heath.borders@gmail.com>
2019-05-14 00:10:07 -0500

    Wrote an espresso test!

---
+++ app/src/androidTest/java/com/github/hborders/heathcast/SearchEspressoTest.java
@@ -0,0 +1,85 @@
+package com.github.hborders.heathcast;
+
+import android.view.KeyEvent;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import com.github.hborders.heathcast.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SearchEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(
+            MainActivity.class,
+            false,
+            true
+    );
+
+    @Test
+    public void searchForPlanetMoneyAndSubscribe() throws Exception {
+        onView(withId(R.id.fragment_main_add_podcast_fab)).perform(click());
+        onView(withId(R.id.fragment_podcast_search_search_view)).perform(
+                typeText("Planet Money"),
+                pressKey(KeyEvent.KEYCODE_ENTER)
+        );
+
+        // Need to replace this with an idling resource
+        Thread.sleep(5 * 1000);
+
+        onView(withId(R.id.fragment_podcast_list_podcasts_recycler_view)).perform(
+                actionOnItemAtPosition(0, click())
+        );
+
+        {
+            final ViewInteraction subscribedButtonViewInteration =
+                    onView(withId(R.id.fragment_podcast_subscribed_button));
+            subscribedButtonViewInteration.check(
+                    matches(
+                            withText(R.string.fragment_podcast_not_subscribed)
+                    )
+            );
+            subscribedButtonViewInteration.perform(
+                    click()
+            );
+            subscribedButtonViewInteration.check(
+                    matches(
+                            withText(R.string.fragment_podcast_subscribed)
+                    )
+            );
+        }
+
+        Espresso.pressBack();
+
+        // Need to replace this with an idling resource
+        Thread.sleep(1000);
+
+        onView(withId(R.id.fragment_podcast_list_podcasts_recycler_view)).perform(
+                actionOnItemAtPosition(0, click())
+        );
+
+        onView(withId(R.id.fragment_podcast_subscribed_button)).check(
+                matches(
+                        withText(R.string.fragment_podcast_subscribed)
+                )
+        );
+    }
+}
